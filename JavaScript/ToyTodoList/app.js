f95fb348dee38a155a301644888a944098fc2367
kimsangjunzzang <84498457+kimsangjunzzang@users.noreply.github.com>
2024-11-16 11:29:53 +0900

    [ToyTodoList] 함수 분리

--- JavaScript/ToyTodoList/app.js
+++ JavaScript/ToyTodoList/app.js
@@ -1,11 +1,11 @@
 const inputField = document.getElementById("inputField");
 const addBtn = document.getElementById("addBtn");
 const deleteBtn = document.getElementById("deleteBtn");
-
 const todoList = document.getElementById("todoList");
 
 // MARK: Add Button Event
-addBtn.addEventListener("click", () => {
+
+function addToItem() {
   const todoContent = document.createElement("li");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
@@ -14,20 +14,31 @@ addBtn.addEventListener("click", () => {
   if (item !== "") {
     todoContent.appendChild(checkbox);
     todoContent.appendChild(document.createTextNode(item));
-
     inputField.value = "";
     todoList.appendChild(todoContent);
   } else {
     alert("빈칸을 채워주세요");
   }
-});
+}
 
-// MARK: Delete Button Event
-deleteBtn.addEventListener("click", () => {
+function deleteToItem() {
   const todoContent = todoList.getElementsByTagName("li");
-  for (let i = 0; i < todoContent.length; i++) {
+  for (let i = todoContent.length - 1; i >= 0; i--) {
     if (todoContent[i].getElementsByTagName("input")[0].checked) {
       todoList.removeChild(todoContent[i]);
     }
   }
-});
+}
+
+// MARK: Delete Button Event
+// deleteBtn.addEventListener("click", () => {
+//   const todoContent = todoList.getElementsByTagName("li");
+//   for (let i = 0; i < todoContent.length; i++) {
+//     if (todoContent[i].getElementsByTagName("input").checked) {
+//       todoList.removeChild(todoContent[i]);
+//     }
+//   }
+// });
+
+addBtn.addEventListener("click", addToItem);
+deleteBtn.addEventListener("click", deleteToItem);
